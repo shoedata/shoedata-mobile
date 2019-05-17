3f1f4138f2a8a7e7d2bf8ae75d56ae469bb4d0ff
envobgm <envobgm@aliyun.com>
2019-05-17 16:31:36 +0800

    init product page

---
+++ src/pages/product/product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-product',
+  templateUrl: 'product.html'
+})
+export class ProductPage {
+
+}
